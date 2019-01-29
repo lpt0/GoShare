521bc3140824ac435aac386d22315c55145368cf
Haran <aunx.so@gmail.com>
2019-01-29 16:13:36 -0500

    Move structs to one file

--- storage/db.go
+++ storage/db.go
@@ -15,6 +15,26 @@ type Storage interface {
 	GetUpload(ID string) (Object, error)
 }
 
+// Object represents an upload on the server, as it is in the database
+type Object struct {
+	ID           string `json:"id"`
+	Date         int64  `json:"date"`
+	Uploader     string `json:"uploader"`
+	Type         Type   `json:"type"`
+	Location     string `json:"location"`
+	OriginalName string `json:"originalName"`
+	MimeType     string `json:"mimeType"`
+}
+
+// Type represents the type of upload
+type Type int
+
+// File = 0, URL = 1
+const (
+	File Type = iota
+	URL
+)
+
 // db is globalized for now
 var db *sql.DB
 
