ccd363c1de492e282448fc3ad95daf87252eb1cd
Haran <aunx.so@gmail.com>
2019-01-29 22:42:48 -0500

    Split random-redirect into function

--- handlers/default.go
+++ handlers/default.go
@@ -13,22 +13,28 @@ import (
 	"github.com/juju/errors"
 )
 
+// Redirect is the function that will redirect a user to a random site specified in the config
+func redirect(w http.ResponseWriter, r *http.Request) {
+	redirect := config.Redirects[rand.Intn(len(config.Redirects))]
+	log.Println("Redirecting to " + redirect)
+	w.Header().Set("Location", redirect)
+	w.WriteHeader(301)
+	return
+}
+
 // Default provides the default route (/)
 func Default(w http.ResponseWriter, r *http.Request) {
 	url := strings.Split(r.URL.Path, "/")
 	id := strings.Split(url[len(url)-1], ".")[0]
 	upload, e := storage.GetUpload(id)
 	if e != nil {
-		redirect := config.Redirects[rand.Intn(len(config.Redirects))]
-		log.Println("Redirecting to " + redirect)
-		w.Header().Set("Location", redirect)
-		w.WriteHeader(301)
+		redirect(w, r)
 		return
 	}
 	if upload.Type == storage.URL {
 		w.Header().Set("Location", upload.Location)
 		w.WriteHeader(301)
-		w.Write(make([]byte, 1))
+		return
 	} else if upload.Type == storage.File {
 		f, e := os.Open(upload.Location)
 		if e != nil {
